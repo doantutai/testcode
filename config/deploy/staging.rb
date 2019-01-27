c81acad535d2510ecb66656cafb4c2e723e67029
Doan Tu Tai <doantutai1995@gmail.com>
2019-01-27 10:40:04 +0700

    add master config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 server '13.59.34.247', user: 'ubuntu', roles: %w{web app db}
 set :deploy_to, "/var/www/testcodelinux/html/#{fetch(:application)}"
 
-set :branch, 'develop'
+set :branch, 'master'
 set :tmp_dir, "/tmp"
